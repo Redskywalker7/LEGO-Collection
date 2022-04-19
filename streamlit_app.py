221a85c1ae17f91c7b15e26a77d82b59ec3942bb
Redskywalker7 <78881317+Redskywalker7@users.noreply.github.com>
2022-04-18 21:26:53 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,7 +75,7 @@ def main():
             st.image('Aayla.png')  
     
     container2 = st.container()
-    col1, col2, col3= st.columns([1,1])
+    col1, col2, col3= st.columns([1,1,1])
     with container2:
         with col1:
             st.image("Obiwan.png")
