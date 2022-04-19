40dfd1fc04b294797970b2c3d39038e2f97838c5
Redskywalker7 <78881317+Redskywalker7@users.noreply.github.com>
2022-04-19 00:21:09 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -107,7 +107,7 @@ def main():
         with col3:
             st.metric(label="Wishlist Count",value=Wish_Count)
         with col4:
-            st.metric(label="Most Expensive Wishlist \n Minifigure", value=print(Wishlist['Name'][maxIndex]  + " (" + str(Wishlist['Brickeconomy Value'][maxIndex]) + ")"))
+            st.write(label=print("Most Expensive Wishlist Minifigure"+ "\n" + Wishlist['Name'][maxIndex]  + "\n" + " (" + str(Wishlist['Brickeconomy Value'][maxIndex]) + ")"))
         
     
     container3 = st.container()
