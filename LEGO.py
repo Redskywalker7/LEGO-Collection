6706fe0fd6606c5b6c746fd20a143da6187be686
Redskywalker7 <78881317+Redskywalker7@users.noreply.github.com>
2022-04-28 08:56:30 -0600

    Update LEGO.py

--- LEGO.py
+++ LEGO.py
@@ -1,6 +1,34 @@
 import pandas as pd
+from pyparsing import lineEnd
 import requests
 from bs4 import BeautifulSoup as bs
+
+
+# To DO:
+# Create .h file for Functions as a test
+# Create conversion table for harry potter subthemes
+
+
+# Function to determine if set or not and retired or not, then scrape value
+def Lego_Value(url):
+    webpage = requests.get(url)
+    soup = bs(webpage.content,"html.parser")
+    if soup.find_all("div", {"class": "col-xs-7"})[7].string == "Retired":
+        Value = soup.find('b').string
+    elif soup.find_all('h4')[8].string == 'Minifig Details':
+        Value = soup.find('b').string
+    else:
+        Value = soup.find_all("div", {"class": "col-xs-7"})[14].string
+    return Value
+
+# Function to get Bricklink ID from Rebrickable link
+def Bricklink_ID(Fig):
+    url = 'https://rebrickable.com/minifigs/'+Fig
+    webpage = requests.get(url)
+    soup = bs(webpage.content,"html.parser")
+    link = str(soup.find_all('td')[15:])
+    link = link[link.find('q=')+2:link.find('&')]
+    return link
 
 # Load in datasets
 sets = pd.read_csv(r"C:\Users\lukejo\Documents\Data\Practice\sets.csv").rename(columns={'name':'Set Name'})
@@ -14,8 +42,8 @@ Inventory = pd.read_csv(r"C:\Users\lukejo\Documents\Data\Practice\My_Collection.
 # Join my Inventory to Rebrickable Database
 My_sets = Inventory.merge(sets,left_on ='Set Number',right_on = 'set_num',how = 'inner')
 My_sets = My_sets.merge(themes,left_on ='Theme_ID',right_on = 'id',how = 'inner')
-My_sets = My_sets[['Set Number','Set Name_y','Theme','year','num_parts','Acquisition', 'Type',
-       'Cost', 'State']].rename(columns={'Set Name_y': 'Set Name','year':'Year','num_parts':'Piece Count'})
+My_sets = My_sets[['Set Number','Set Name_y','Theme Name','year','num_parts','Acquisition', 'Type',
+       'Cost', 'State']].rename(columns={'Set Name_y': 'Set Name','year':'Year','num_parts':'Piece Count', "Theme Name":'Theme'})
 
 # Join my Inventory to Rebrickable Database to create list of minifigures
 the_inventories = My_sets.merge(inventories,left_on ='Set Number',right_on = 'set_num',how = 'inner')
@@ -25,20 +53,7 @@ My_Minifigs = My_Minifigs.merge(minifigs,left_on ='fig_num',right_on = 'fig_num'
 My_Minifigs = My_Minifigs[['fig_num','name', 'quantity',
        'Theme']].rename(columns={'fig_num':"Rebrickable ID",'name':'Name', 'quantity':'Quantity'})
 
-# Test sets or Minifigs
-Pod = 'https://www.brickeconomy.com/set/75258-1/lego-star-wars-anakins-podracer-20th-anniversary-edition'
-Falcon = 'https://www.brickeconomy.com/set/75192-1/lego-star-wars-millennium-falcon'
-Looney = 'https://www.brickeconomy.com/set/71030-3/lego-minifigure-series-looney-tunes-wile-e-coyote'
-Queen = 'https://www.brickeconomy.com/minifig/sw0387/queen-amidala'
 
-# Determine if set or not and retired or not, then scrape value
-webpage = requests.get(Queen)
-soup = bs(webpage.content,"html.parser")
-if soup.find_all("div", {"class": "col-xs-7"})[7].string == "Retired":
-    Value = soup.find('b').string
-elif soup.find_all('h4')[8].string == 'Minifig Details':
-    Value = soup.find('b').string
-else:
-    Value = soup.find_all("div", {"class": "col-xs-7"})[14].string
-Value
+# Get Minifig Value from table
+Lego_Value('https://www.brickeconomy.com/minifig/'+Bricklink_ID(My_Minifigs['Rebrickable ID'][35]))
 
