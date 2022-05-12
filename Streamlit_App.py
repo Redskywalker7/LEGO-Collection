4747b294915a1e4dea051edb163b70bff16e485f
Redskywalker7 <78881317+Redskywalker7@users.noreply.github.com>
2022-05-12 15:31:53 -0600

    Update Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -1,32 +1,40 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-#LIBRARIES
 import pandas as pd
+import LEGO_Datasets as LD
+import LEGO_Functions as LF
+import LEGO_Visuals as LV
 import dash
 from dash import dcc
 from dash import html
 import plotly.express as px
 import plotly.graph_objects as go
 import streamlit as st
-#from datetime import datetime, timedelta, date
 
+# FUNCTIONS
+def Dollar_Format(x):
+  return "${:,.2f}".format(x)
+colours = ( "cadetblue", "turquoise", "skyblue",
+          "lightsteelblue","azure","teal")
 
 # STREAMLIT SETUP
 st.set_page_config(
-    page_title="LEGO Star Wars",
+    page_title="LEGO Collection Analysis",
     layout="wide",
     initial_sidebar_state="collapsed",
 )
 
 def main():
     container1 = st.container()
-    col1, col2 = st.columns([5,15])
+    col1, col2 = st.columns([5,5,20])
     with container1:
         with col1:
             st.image('Pictures/sw0833.png')
         with col2:
             st.image('Pictures/Aayla.jpg')  
+        with col3:
+            st.plotly_chart(LV.Set_Minifig_Values_Table)
   
 if __name__ == "__main__":
     main()
