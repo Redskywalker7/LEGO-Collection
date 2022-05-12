625cad50e958ba2c06127fb20077ab23b67c6c83
Redskywalker7 <78881317+Redskywalker7@users.noreply.github.com>
2022-05-12 15:34:13 -0600

    Update Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -27,14 +27,14 @@ st.set_page_config(
 
 def main():
     container1 = st.container()
-    col1, col2 = st.columns([5,5,20])
+    col1, col2 = st.columns([5,5])
     with container1:
         with col1:
             st.image('Pictures/sw0833.png')
         with col2:
             st.image('Pictures/Aayla.jpg')  
-        with col3:
-            st.plotly_chart(LV.Set_Minifig_Values_Table)
+            
+LD.Wishlist['Name'][0]
   
 if __name__ == "__main__":
     main()
