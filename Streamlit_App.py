c4140cc190ecd9b7a8b3fec868c3162f142a51b1
Redskywalker7 <78881317+Redskywalker7@users.noreply.github.com>
2022-05-13 11:57:15 -0600

    Update Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -23,7 +23,7 @@ def main():
             
 choice = st.text_input(label = "Enter set or minifigure ID",placeholder = "75020")
 
-st.plotly_chart(LV.tablefunc(75020),use_container_width=True)
+st.plotly_chart(LV.tablefunc(choice),use_container_width=True)
   
 if __name__ == "__main__":
     main()
