33d3dfe870ff1db0010392add3f73737030ec295
Redskywalker7 <78881317+Redskywalker7@users.noreply.github.com>
2022-05-13 09:08:35 -0600

    Update LEGO_Visuals.py

--- LEGO_Visuals.py
+++ LEGO_Visuals.py
@@ -7,26 +7,9 @@ from dash import html
 import plotly.express as px
 import plotly.graph_objects as go
 
-#def table_func(set_num):
-#    del table
-#    global table
-#    table = LF.Set_Minifig_Values(set_num)
-
-
-#TABLES
-# Main Set List Table
-#Set_Minifig_Values_Table = go.Figure(data=[go.Table(
-#    columnwidth = [2,2,5,1,2],
-#    header=dict(values=("<b>Rebrickable ID</b>","<b>Bricklink ID</b>","<b>Minifig</b>","<b>Quantity</b>","<b>Value</b>"),
-#                #fill_color=colours[0],
-#                align='center'),
-#    cells=dict(values=[table['Rebrickable ID'],table['Bricklink ID'],table['Minifig'],table['Quantity'],table['Value']],
-#               #fill_color=colours[1],
-#               align=['left','left','left','center','right']))
-#])
-
 def tablefunc(set_num):
     table = LF.Set_Minifig_Values(set_num)
+
     Set_Minifig_Values_Table = go.Figure(data=[go.Table(
     columnwidth = [2,2,5,1,2],
     header=dict(values=("<b>Rebrickable ID</b>","<b>Bricklink ID</b>","<b>Minifig</b>","<b>Quantity</b>","<b>Value</b>"),
