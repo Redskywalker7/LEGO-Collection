fa68bb9c1fddab03859b7cdff8f02a25e3908553
Redskywalker7 <78881317+Redskywalker7@users.noreply.github.com>
2022-05-13 16:13:50 -0600

    Update LEGO_Visuals.py

--- LEGO_Visuals.py
+++ LEGO_Visuals.py
@@ -9,7 +9,6 @@ import plotly.graph_objects as go
 
 def tablefunc(set_num):
     table = LF.Set_Minifig_Values(set_num)
-
     Set_Minifig_Values_Table = go.Figure(data=[go.Table(
     columnwidth = [2,2,5,1,2],
     header=dict(values=("<b>Rebrickable ID</b>","<b>Bricklink ID</b>","<b>Minifig</b>","<b>Quantity</b>","<b>Value</b>"),
