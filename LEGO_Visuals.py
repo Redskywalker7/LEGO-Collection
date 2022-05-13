e57b3d11f5e34ad531e0ba0b49eff07e1dbc5dca
Redskywalker7 <78881317+Redskywalker7@users.noreply.github.com>
2022-05-13 09:06:28 -0600

    Update LEGO_Visuals.py

--- LEGO_Visuals.py
+++ LEGO_Visuals.py
@@ -7,9 +7,10 @@ from dash import html
 import plotly.express as px
 import plotly.graph_objects as go
 
-def table_func(set_num):
-    global table
-    table = LF.Set_Minifig_Values(set_num)
+#def table_func(set_num):
+#    del table
+#    global table
+#    table = LF.Set_Minifig_Values(set_num)
 
 
 #TABLES
@@ -23,3 +24,17 @@ Set_Minifig_Values_Table = go.Figure(data=[go.Table(
                #fill_color=colours[1],
                align=['left','left','left','center','right']))
 ])
+
+def tablefunc(set_num):
+    table = LF.Set_Minifig_Values(set_num)
+    Set_Minifig_Values_Table = go.Figure(data=[go.Table(
+    columnwidth = [2,2,5,1,2],
+    header=dict(values=("<b>Rebrickable ID</b>","<b>Bricklink ID</b>","<b>Minifig</b>","<b>Quantity</b>","<b>Value</b>"),
+                #fill_color=colours[0],
+                align='center'),
+    cells=dict(values=[table['Rebrickable ID'],table['Bricklink ID'],table['Minifig'],table['Quantity'],table['Value']],
+               #fill_color=colours[1],
+               align=['left','left','left','center','right']))
+    ])
+    return Set_Minifig_Values_Table
+
