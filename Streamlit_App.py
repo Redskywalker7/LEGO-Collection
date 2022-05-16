f6381bbe9153ada166b632f82a73ac92c75a97c6
Redskywalker7 <78881317+Redskywalker7@users.noreply.github.com>
2022-05-16 08:18:38 -0600

    Update Streamlit_App.py

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -24,7 +24,6 @@ def main():
     #st.metric(label = "What are we getting here", value = choice)
     #st.plotly_chart(LV.tablefunc(choice),use_container_width=True) 
     choice = ''
-    set_val = 0 
     choice = st.text_input(label = "Enter set or minifigure ID",placeholder = '75020')
     #st.metric(label = "Set Value", value = LF.set_val)
     if len(choice) > 0:
@@ -33,8 +32,6 @@ def main():
         st.metric(label = "Set Value", value = LF.set_val) 
         st.metric(label = "Minifigs Total Value", value = ((table.Quantity*table.Value).sum())) 
         st.plotly_chart(set_table,use_container_width=True) 
-        
-    
-  
+            
 if __name__ == "__main__":
     main()
