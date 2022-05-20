61aa3b469a83d9429cd81926cc245cf3300217a0
Redskywalker7 <78881317+Redskywalker7@users.noreply.github.com>
2022-05-20 08:58:05 -0600

    Update LEGO_Datasets.py

--- LEGO_Datasets.py
+++ LEGO_Datasets.py
@@ -12,14 +12,14 @@ import pandas as pd
 
 # Load in datasets
 
-sets = pd.read_csv("Data\sets.csv").rename(columns={'name':'Set Name'})
-themes = pd.read_csv("Data\themes.csv").rename(columns={'name':'Theme Name'})
-minifigs = pd.read_csv("Data\minifigs.csv")
-minifigs_inv = pd.read_csv("Data\inventory_minifigs.csv")
-inventories = pd.read_csv("Data\inventories.csv")
-inventory_minifigs = pd.read_csv("Data\inventory_minifigs.csv")
-Inventory = pd.read_csv("Data\My_Collection.csv")
-Wishlist = pd.read_csv("Data\Minifig Wishlist.csv")
+sets = pd.read_csv("Data/sets.csv").rename(columns={'name':'Set Name'})
+themes = pd.read_csv("Data/themes.csv").rename(columns={'name':'Theme Name'})
+minifigs = pd.read_csv("Data/minifigs.csv")
+minifigs_inv = pd.read_csv("Data/inventory_minifigs.csv")
+inventories = pd.read_csv("Data/inventories.csv")
+inventory_minifigs = pd.read_csv("Data/inventory_minifigs.csv")
+Inventory = pd.read_csv("Data/My_Collection.csv")
+Wishlist = pd.read_csv("Data/Minifig Wishlist.csv")
 Wishlist['Set Number'] = Wishlist['Set Number'].fillna(0).astype(int)
 
 # Join my Inventory to Rebrickable Database
