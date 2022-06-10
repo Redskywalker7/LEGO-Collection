912a7f27ea159b04b01daf04196ae7c2fb256d1c
Redskywalker7 <78881317+Redskywalker7@users.noreply.github.com>
2022-06-10 15:14:40 -0600

    Update LEGO_Visuals.py

--- LEGO_Visuals.py
+++ LEGO_Visuals.py
@@ -20,11 +20,11 @@ def tablefunc(table):
     return Set_Minifig_Values_Table
 
 Wishlist_Table = Set_Minifig_Values_Table = go.Figure(data=[go.Table(
-    columnwidth = [2,2,5,1,2,1],
-    header=dict(values=("<b>Bricklink ID</b>","<b>Rebrickable ID</b>","<b>Minifigure</b>","<b>Theme</b>","<b>Contained Set</b>","<b>Value</b>"),
+    columnwidth = [2,5,2,2],
+    header=dict(values=("<b>Bricklink ID</b>","<b>Minifigure</b>","<b>Contained Set</b>","<b>Value</b>"),
                 #fill_color=colours[0],
                 align='center'),
-    cells=dict(values=[LD.Wishlist_with_vals['Minifig Number'],LD.Wishlist_with_vals['Rebrickable ID'],LD.Wishlist_with_vals['Name'],LD.Wishlist_with_vals['Theme'],LD.Wishlist_with_vals['Set Number'],LD.Wishlist['Minifig Value']],
+    cells=dict(values=[LD.Wishlist_with_vals['Minifig Number'],LD.Wishlist_with_vals['Name'],LD.Wishlist_with_vals['Set Number'],LD.Wishlist['Minifig Value']],
                #fill_color=colours[1],
-               align=['left','left','left','center','right','center']))
+               align=['left','left','center','center']))
     ])
